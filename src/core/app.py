839b99bdb3e0f3e569c658dde6770bc0c9ff1775
fjed <d.ostapczuk1@gmail.com>
2024-03-28 11:24:33 +0100

    Add CORS middleware

--- src/core/app.py
+++ src/core/app.py
@@ -1,32 +1,44 @@
 from contextlib import asynccontextmanager
+from typing import List
 
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .auth.jwt_handler import AuthHandler
-from .database import init_db
+from .database import Database
 from .routes.auth_routes import router as AuthRouter
 from .routes.category_routes import router as CategoryRouter
 from .routes.part_routes import router as PartsRouter
 from .routes.search_routes import router as SearchRouter
 
+db: Database = Database()
+auth_handler: AuthHandler = AuthHandler()
+
 
 @asynccontextmanager
 async def lifespan(fastapi: FastAPI):
     print("Initializing database...")
-    await init_db()
+    await db.init_db()
     yield
     print("Closing connection...")
-    # add
+    db.close_db()
 
 
-auth_handler: AuthHandler = AuthHandler()
-app: FastAPI = FastAPI(lifespan=lifespan)
+app: FastAPI = FastAPI(title="Parts warehouse API", lifespan=lifespan)
 
 
-@app.get("/", tags=["Root"])
-async def read_root():
-    return {"message": "Parts warehouse"}
+origins: List[str] = [
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,  # type: ignore
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET, POST, PUT, DELETE"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     PartsRouter,
@@ -47,3 +59,8 @@ app.include_router(
     dependencies=[Depends(auth_handler.verify_token)],
 )
 app.include_router(AuthRouter, tags=["Auth"], prefix="/auth")
+
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Parts warehouse API"}
--- src/core/database.py
+++ src/core/database.py
@@ -9,8 +9,13 @@ MONGO_URL: str = settings.MONGO_URL
 DB_NAME: str = settings.DB_NAME
 
 
-async def init_db() -> None:
-    client = AsyncIOMotorClient(f"{MONGO_URL}/{DB_NAME}")
-    await init_beanie(
-        database=client.get_default_database(), document_models=models.__all__
-    )
+class Database:
+    client: AsyncIOMotorClient = AsyncIOMotorClient(f"{MONGO_URL}/{DB_NAME}")
+
+    async def init_db(self) -> None:
+        await init_beanie(
+            database=self.client.get_default_database(), document_models=models.__all__
+        )
+
+    def close_db(self) -> None:
+        self.client.close()
